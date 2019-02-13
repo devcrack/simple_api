d8b5a4f665f791c33170e34f3b36aa5ab810ec6f
devcrack <jesusaureliohernandez@alumnos.uaslp.edu.mx>
2019-02-12 18:29:42 -0600

    adding testing config

--- app/main/config.py
+++ app/main/config.py
@@ -6,3 +6,16 @@ class Config:
     secret_key = os.getenv('SECRET_KEY', 'mientras123' )
     DEBUG = False
 
+class Development_Config(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'flask_boilerplate_main.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Testing_Config(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'flask_boilerplate_test.db')
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
